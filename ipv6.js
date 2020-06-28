3aa1d53194fec0321587e51e8e18fa828a215d0c
sudhakar5891 <sudhakar.purushothaman@centurylink.com>
2020-06-28 23:41:16 +0000

    Lab 3

--- ipv6.js
+++ ipv6.js
@@ -1,19 +1,3 @@
-/*
-  Import the built-in path module.
-  See https://nodejs.org/api/path.html
-  The path module provides utilities for working with file and directory paths.
-  IAP requires the path module to access local file modules.
-  The path module exports an object.
-  Assign the imported object to variable path.
-*/
-const path = require('path');
-
-/**
- * Import helper function module located in the same directory
- * as this module. IAP requires the path object's join method
- * to unequivocally locate the file module.
- */
-const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
 module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
 /**
  * Calculates an IPv4-mapped IPv6 address.
